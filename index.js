240efb37d8a24a0f6ddf8b6e1bb76db12b1a8ded
hzhuangdi <kisscat1990114@163.com>
2017-08-18 18:31:12 +0800

    暂时去掉参数解析功能

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var loaderUtils = require("loader-utils");
 module.exports = function(content) {
     this.cacheable && this.cacheable();
 
-    var query = loaderUtils.parseQuery(this.query) || {};
+    var query = {};
     var tpl = content || '';
 
     tpl = cleanRedundantCode(tpl, query);// remove redundant code
